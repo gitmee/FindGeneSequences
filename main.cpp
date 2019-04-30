039452e3fe8bee7c5943836458b5bb16aadcb949
Fion Chan <fion.sun@gmail.com>
2019-04-29 21:24:27 -0700

    checks gene for start and stop codons, bug fixes, documentation & readability

--- main.cpp
+++ main.cpp
@@ -1,55 +1,95 @@
-/** Displays gene sequences from a genome string
-    Author: Fion Chan
+/** ------------------------------------------------------------------------
+    FindGeneSequences
+    main.cpp
+    Purpose: Displays possible gene sequences from a genome string.
 
-    TODO:
-        - Add check to exclude sequences that include stop and start codons
-        - Optimize/encapsulate code
-*/
+    @author Fion Chan
+    @since Apr 29, 2019
+
+    Notes:
+        - Output gene sequences exclude start and stop codons
+        - Frameshifts are not predicted
+        - UTRs are not removed
+--------------------------------------------------------------------------*/
 
 #include <iostream>
 #include <string>
 
 using namespace std;
 
+
+// VARIABLES
+static const string STOP_CODONS[3] = { "TAG", "TAA", "TGA" };
+
+// FUNCTIONS
+bool verifyGene(const string& sequence);
+string geneBuilder(int start, int stop, const string& sequence);
+void geneFinder(const string& sequence);
+
+
 int main()
 {
     string genome;
 
-	cout << "Enter a genome string: ";
+	cout << "Enter a DNA string:" << endl;
     getline(cin, genome);
 
-    int start = genome.find("ATG");
-    int stop, length;
-    string gene("");
-
-    while (start > -1) {
-        //cout << "start: " << start << endl;
-        stop = genome.find("TAG", start + 3);
-        if (stop > -1) {
-            //cout << "stop: " << stop << endl;
-            length = stop - start - 3;
-            gene = genome.substr(start + 3, length);
-            if (gene.length() % 3 == 0)
-                cout << "at TAG: " << gene << endl;
-        }
-        stop = genome.find("TGA", start + 3);
-        if (stop > -1) {
-            length = stop - start - 3;
-            //cout << "stop: " << stop << endl;
-            gene = genome.substr(start + 3, length);
-            if (gene.length() % 3 == 0)
-                cout << "at TGA: " <<  gene << endl;
-        }
-        stop = genome.find("TAA", start + 3);
-        if (stop > -1) {
-            //cout << "stop: " << stop << endl;
-            length = stop - start - 3;
-            gene = genome.substr(start + 3, length);
-            if (gene.length() % 3 == 0)
-                cout << "at TAA: " <<  gene << endl;
-        }
-        start = genome.find("ATG", start + 1);
-    }
+    cout << "Possible gene sequences found:" << endl;
+    geneFinder(genome);
 
     return 0;
+}
+
+
+/**
+    Purpose: Checks the sequence for nucleotide length divisible by 3,
+    and checks for the exclusion of start and stop codons.
+    @param sequence is a DNA sequence
+    @return true if sequence satisfies described purpose
+    @return false if sequence does not satisfy described purpose
+*/
+bool verifyGene(const string& sequence) {
+    if ((sequence.length() % 3) != 0) return false;
+    if (sequence.find("ATG") != -1) return false;
+    for (int i = 0; i < STOP_CODONS->size(); i++) {
+        if (sequence.find(STOP_CODONS[i]) != -1)
+            return false;
+    }
+    return true;
+}
+
+/**
+    Purpose: Builds a potential gene string, and checks it using verifyGene.
+    @param start is the start codon's starting position
+    @param stop is the stop codon's starting position
+    @param sequence is a DNA sequence
+    @return gene string if verifyGene returns true
+    @return empty string if verifyGene returns false
+*/
+string geneBuilder(int start, int stop, const string& sequence) {
+    int length = stop - start - 3;
+    string gene = sequence.substr(start + 3, length);
+    if (verifyGene(gene)) return gene;
+    return "";
+}
+
+/**
+    Purpose: Searches for and prints possible gene sequences from input.
+    @param sequence is a DNA sequence
+*/
+void geneFinder(const string& sequence) {
+    int start = sequence.find("ATG");
+    int stop;
+    string gene;
+
+    while (start != -1) {
+        for (int i = 0; i < STOP_CODONS->size(); i++) {
+            stop = sequence.find(STOP_CODONS[i], start + 3);
+            if (stop != -1) {
+                gene = geneBuilder(start, stop, sequence);
+                if (!gene.empty()) cout << gene << endl;
+            }
+        }
+        start = sequence.find("ATG", start + 1);
+    }
 }
